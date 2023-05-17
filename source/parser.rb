f6247ea3bee191af773509ce1ec01e7c432009ab
Pamela Glickman <pamela@Pamelas-MacBook-Pro.local>
2023-05-17 10:18:43 +1000

    Updated parser and commands

--- source/parser.rb
+++ source/parser.rb
@@ -3,35 +3,34 @@ require_relative 'robot.rb'
  module Parser
   def parse_and_run_commands(command_data, robot)
     command_data.each do |command|
-      formatted_command = command.upcase.gsub(' ','')
-      if formatted_command.match?(/PLACE/)
-        parse_and_run_place_command(formatted_command, robot)
-      else  
-        run_non_place_command(formatted_command, robot)
-      end   
+      formatted_command = command.match(/[a-zA-z]*/).to_s
+      case formatted_command
+      when 'PLACE'
+        place_data = parse_place_command(command)
+        robot.place(place_data[:x_position], place_data[:y_position], place_data[:facing_direction])
+      when 'MOVE'
+        robot.move
+      when 'LEFT'
+        robot.left
+      when 'RIGHT'
+        robot.right 
+      when 'REPORT'
+        robot.report
+      else
+        puts "Invalid command: #{command}"    
+      end  
     end
   end
 
-  def parse_and_run_place_command(command, robot)
+  def parse_place_command(command)
     place_command_data = command.split(',')
     x_position = place_command_data[0].match(/\d+/).to_s.to_i
     y_position = place_command_data[1].match(/\d+/).to_s.to_i
-    facing_direction = place_command_data[2].upcase
-    robot.place(x_position, y_position, facing_direction)
-  end
-
-  def run_non_place_command(command, robot)
-    case command
-    when 'MOVE'
-      robot.move
-    when 'LEFT'
-      robot.left
-    when 'RIGHT'
-      robot.right 
-    when 'REPORT'
-      robot.report
-    else
-      puts "Invalid command: #{command}"    
-    end
+    facing_direction = place_command_data[2].strip.upcase
+    { 
+      x_position: x_position, 
+      y_position: y_position, 
+      facing_direction: facing_direction 
+    }
   end
 end
